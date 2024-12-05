cb5b43a838ec85da1f0a99c08663e7982c558728
jmodhiambo <jmodhiambo9346@gmail.com>
2024-12-05 20:30:52 +0300

    Task 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -5,7 +5,7 @@
  * binary_tree_inorder - Goes through a binary tree using in-order traversal.
  * @tree: A pointer to the root node of the tree to traverse.
  * @func: A pointer to a function to call for each node.
- * 
+ *
  * Return: Void.
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
